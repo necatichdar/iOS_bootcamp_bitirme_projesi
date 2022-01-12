2326ceddf42b9d455975a90ab40812c093e7aeb0
necatichdar <necaticuhadar06@gmail.com>
2022-01-12 10:59:02 +0300

    Initial Commit

---
+++ BitirmeProjesi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitirmeProjesi
+//
+//  Created by Necati Çuhadar on 12.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
